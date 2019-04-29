232a5e3183dbca479d9b660b7202b73bc22f8fb1
Roald Nefs <info@roaldnefs.com>
2019-04-29 14:54:27 +0200

    Bump version to v0.0.2
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="meteoalertapi",
-    version="0.0.1",
+    version="0.0.2",
     author="Rolf Berkenbosch",
     author_email="rolf@berkenbosch.nl",
     description="A small api to get alerting messages from extreme weather in Europe.",
