6a300286226092cd90b2c1ce4d3e4aa50c723286
Ronan Murray <2470586+ronanmu@users.noreply.github.com>
2022-12-08 01:10:29 +0000

    Updates to support matching areas based on their statistical code as well as area name

--- tests/test_meteoalertapi.py
+++ tests/test_meteoalertapi.py
@@ -1,6 +1,7 @@
 # - *- coding: utf- 8 - *-
 import requests_mock
 import unittest
+import os
 from meteoalertapi import *
 
 class TestMeteoalert(unittest.TestCase):
@@ -9,8 +10,15 @@ class TestMeteoalert(unittest.TestCase):
         pass
 
 
-    def test_get_alert(self):
-        self.meteo = Meteoalert('netherlands', 'Groningen', 'ne-NL')
+    @requests_mock.mock()
+    def test_get_alert(self, m):
+        with open(self._file_path('uk_sample.xml')) as xml_file:
+            m.register_uri('GET', '/feeds/meteoalarm-legacy-atom-united-kingdom', text=xml_file.read(), status_code=200)
+        with open(self._file_path('uk_nothern_ireland_sample_detail.xml')) as xml_file:
+            m.register_uri('GET', '/api/v1/warnings/feeds-united-kingdom/a4647d38-40ea-4c01-b874-4e2b3df383b9',
+                           text=xml_file.read(), status_code=200)
+
+        self.meteo = Meteoalert('United-Kingdom', 'Northern Ireland')
 
         """Should return a dict (maybe empty)"""
         try:
@@ -18,6 +26,7 @@ class TestMeteoalert(unittest.TestCase):
         except Exception:
             self.fail("'get_alert()' raised an unexpected exception!")
         self.assertEqual(type(data), dict)
+        self.assertGreater(len(data), 0)
 
 
     def test_wrong_country_name(self):
@@ -26,6 +35,52 @@ class TestMeteoalert(unittest.TestCase):
         """Should throw WrongCountry exception"""
         self.assertRaises(WrongCountry, self.meteo.get_alert)
 
+    @requests_mock.mock()
+    def test_retrieve_alert_by_code(self, m):
+        with open(self._file_path('ireland_sample.xml')) as xml_file:
+            m.register_uri('GET', '/feeds/meteoalarm-legacy-atom-ireland', text=xml_file.read(), status_code=200)
+        with open(self._file_path('ireland_sample_detail.xml')) as xml_file:
+            m.register_uri('GET', '/api/v1/warnings/feeds-ireland/558830a2-74a1-402a-915c-ab365ec68d25', text=xml_file.read(), status_code=200)
+
+        self.meteo = Meteoalert('Ireland', 'EI25')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertGreater(len(data), 0)
+
+        self.meteo = Meteoalert('Ireland', 'FAKE')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertEqual(len(data), 0)
+
+    @requests_mock.mock()
+    def test_lookup_by_code_and_area_name(self, m):
+        with open(self._file_path('france_sample.xml')) as xml_file:
+            m.register_uri('GET', '/feeds/meteoalarm-legacy-atom-france', text=xml_file.read(), status_code=200)
+        with open(self._file_path('france_sample_detail.xml')) as xml_file:
+            m.register_uri('GET', '/api/v1/warnings/feeds-france/04d497d4-d114-4d6b-93ba-13fa7ed577fe',
+                           text=xml_file.read(), status_code=200)
+
+        self.meteo = Meteoalert('France', 'Haute Savoie')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertGreater(len(data), 0)
+
+        # ensure we can get the French localised version
+        self.meteo = Meteoalert('France', 'Haute Savoie', 'fr-FR')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertGreater(len(data), 0)
+
+        # ensure we can get the alert by area code
+        self.meteo = Meteoalert('France', 'FR822', 'fr-FR')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertGreater(len(data), 0)
+
+        self.meteo = Meteoalert('France', 'FAKE')
+        data = self.meteo.get_alert()
+        self.assertEqual(type(data), dict)
+        self.assertEqual(len(data), 0)
 
     def test_server_not_reachable(self):
         self.meteo = Meteoalert('poland', 'krakow', 'en-US')
@@ -43,6 +98,10 @@ class TestMeteoalert(unittest.TestCase):
             """Should throw ServerError exception"""
             self.assertRaises(ServerError, self.meteo.get_alert)
 
+    @staticmethod
+    def _file_path(file_name):
+        thispath = os.path.dirname(__file__)
+        return "{}/{}".format(thispath, file_name)
 
 if __name__ == '__main__':
     unittest.main()
